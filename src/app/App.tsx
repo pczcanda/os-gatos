e9da174387f8d3d519eb09d73296ecbf74074ec3
Paciência Canda <paciencia@relashe.com>
2024-09-23 11:32:53 +0100

    [R1] - base routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,25 @@
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import "./App.css";
 
-function App() {
+const App: React.FC<{}> = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Os Gatos</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          element={
+            <div className="App">
+              <header className="App-header">
+                <h1>Os Gatos</h1>
+              </header>
+              <main className="app">
+                <Outlet />
+              </main>
+            </div>
+          }
+        ></Route>
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
