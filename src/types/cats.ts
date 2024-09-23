5beb9e134fe8ede060f2f8043e342d976c03be04
Paciência Canda <paciencia@relashe.com>
2024-09-23 13:13:48 +0100

    [R1] - cats types

---
+++ src/types/cats.ts
@@ -0,0 +1,20 @@
+export interface Cat {
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+  mime_type: string;
+  breeds: Array<{}>;
+  categories: string[];
+}
+
+export interface NewCatResponse {
+  id: string;
+  url: string;
+  sub_id: string;
+  width: number;
+  height: number;
+  original_filename: string;
+  pending: 0 | 1;
+  approved: 0 | 1;
+}
