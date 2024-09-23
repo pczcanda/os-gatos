991abaaab3a6fa667f9436a3160bc5115e0fbab0
Paciência Canda <paciencia@relashe.com>
2024-09-23 15:45:28 +0100

    [R3] - favourite cats API endpoint

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -5,4 +5,5 @@ export const API = "https://api.thecatapi.com/v1/images";
 export const ENDPOINTS = {
   UPLOAD: `${API}/upload`,
   ALL_CATS: `${API}/?limit=100`,
+  FAVOURITE_CATS: `${API}/favourites`,
 };
