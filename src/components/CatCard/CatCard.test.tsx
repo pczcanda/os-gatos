b3db25d1f31fc2f237c54993bb3961e52fb0b3e5
Paciência Canda <paciencia@relashe.com>
2024-09-23 16:38:07 +0100

    [R3] - handle setting cat to favourite

--- src/components/CatCard/CatCard.test.tsx
+++ src/components/CatCard/CatCard.test.tsx
@@ -21,4 +21,23 @@ describe("<CatCard /> cat details", () => {
     const CatImage = screen.getByRole("img");
     expect(CatImage).toHaveAttribute("src", cat.url);
   });
+
+  test("displays cat as favourite", () => {
+    const cat: Cat = {
+      breeds: [],
+      id: "AluKZKrdt",
+      url: "https://cdn2.thecatapi.com/images/AluKZKrdt.jpg",
+      width: 3000,
+      height: 3999,
+      sub_id: "my-user-1",
+      created_at: "2024-09-23T13:05:51.000Z",
+      original_filename: "gato 1.jpeg",
+      breed_ids: null,
+    };
+
+    render(<CatCard cat={cat} isFavourite />);
+
+    const FavIcon = screen.getByLabelText("add to favorites");
+    expect(FavIcon).toBeInTheDocument();
+  });
 });
--- src/components/CatCard/CatCard.tsx
+++ src/components/CatCard/CatCard.tsx
@@ -1,10 +1,27 @@
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import { Card, CardActions, CardMedia, IconButton } from "@mui/material";
 import { CatCardProps } from "./CatCardProps";
+import { favouriteACat } from "../../utils";
+import { useState } from "react";
 
 const CatCard: React.FC<CatCardProps> = ({ cat, isFavourite = false }) => {
+  /* state */
+  const [favourited, setFavourited] = useState<boolean>(isFavourite);
+
   /* events */
-  const handleFavouriting = () => {};
+  const handleFavouriting = async () => {
+    try {
+      if (favourited) {
+      } else {
+        const favouriteCatDetails = await favouriteACat(cat.id);
+        if (favouriteCatDetails.message === "SUCCESS") {
+          setFavourited(true);
+        }
+      }
+    } catch (e) {
+      setFavourited(false);
+    }
+  };
 
   return (
     <Card>
@@ -19,7 +36,7 @@ const CatCard: React.FC<CatCardProps> = ({ cat, isFavourite = false }) => {
       )}
       <CardActions>
         <IconButton aria-label="add to favorites" onClick={handleFavouriting}>
-          <FavoriteIcon color={isFavourite ? "error" : undefined} />
+          <FavoriteIcon color={favourited ? "error" : undefined} />
         </IconButton>
       </CardActions>
     </Card>
