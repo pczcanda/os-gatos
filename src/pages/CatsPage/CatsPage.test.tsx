b742aaff4da322645092f501cf0edd032d371880
Paciência Canda <paciencia@relashe.com>
2024-09-23 14:36:19 +0100

    [R2] - Test: renders list of cats

---
+++ src/pages/CatsPage/CatsPage.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import CatsPage from "./CatsPage";
+
+describe("Cats Listing Page", () => {
+  test("renders list of Cats", async () => {
+    window.fetch = jest.fn();
+    (window.fetch as jest.Mock).mockResolvedValueOnce({
+      json: async () => [
+        {
+          id: "ebv",
+          url: "https://cdn2.thecatapi.com/images/ebv.jpg",
+          width: 176,
+          height: 540,
+          breeds: [],
+          favourite: {},
+        },
+      ],
+    });
+
+    render(<CatsPage />, { wrapper: BrowserRouter });
+
+    const listItems = await screen.findAllByRole("listitem");
+
+    expect(listItems).not.toHaveLength(0);
+  });
+});
