08111df4125e3befdcb750804af9b851957a60f3
Paciência Canda <paciencia@relashe.com>
2024-09-23 12:55:52 +0100

    [R1] - API constants and upload endpoint

---
+++ src/constants/api.ts
@@ -0,0 +1,7 @@
+export const API_KEY = `live_ZIfWpCYrAAdXobhNXtim5zvgoYexRHP8ytMRpCyiLyriUpsyfzo0EaUFt2zORfCc`;
+
+export const API = "https://api.thecatapi.com/v1/images/";
+
+export const ENDPOINTS = {
+  UPLOAD: `${API}/upload`,
+};
