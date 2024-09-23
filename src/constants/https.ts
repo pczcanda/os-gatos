7ff5e59dc0e7e08af4f90a710acb06b99ed351d6
Paciência Canda <paciencia@relashe.com>
2024-09-23 13:14:14 +0100

    [R1] - upload cat image util function

---
+++ src/constants/https.ts
@@ -0,0 +1,5 @@
+import { API_KEY } from "./api";
+
+export const HTTPS_HEADERS = {
+  "x-api-key": API_KEY,
+};
---
+++ src/utils/https.ts
@@ -0,0 +1,20 @@
+import { ENDPOINTS, HTTPS_HEADERS } from "../constants";
+import { NewCatResponse } from "../types/cats";
+
+export const uploadNewCat = async (catImage: FormData) => {
+  const response = await fetch(ENDPOINTS.UPLOAD, {
+    method: "POST",
+    headers: {
+      ...HTTPS_HEADERS,
+      "Content-Type": "multipart/form-data",
+    },
+    body: catImage,
+  });
+
+  if (!response.ok) {
+    throw new Error("failed to upload new cat picture through the API");
+  }
+  const data: NewCatResponse = await response.json();
+
+  return data;
+};
